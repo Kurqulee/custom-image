fb306a9c926f87a29ba6d6d7d7779b3b926566ff
Kurqulee <167604296+Kurqulee@users.noreply.github.com>
2025-03-02 16:21:51 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,6 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf install -y tmux 
-
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -19,6 +16,18 @@ dnf install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+# enable repos
+dnf5 -y config-manager addrepo --overwrite --from-repofile=https://repo.secureblue.dev/secureblue.repo
+dnf5 -y copr enable secureblue/trivalent
+
+# install packages
+# TODO: replace virt-manager system package with flatpak once USB drives can be passed through
+# TODO: Look into making emacs a sysext once Fedora 42 lands
+dnf5 -y install trivalent
+dnf5 -y install trivalent-subresource-filter
+dnf5 -y install virt-manager
+dnf5 -y install emacs
+
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
